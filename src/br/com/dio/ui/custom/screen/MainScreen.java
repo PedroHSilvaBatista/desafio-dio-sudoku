386ae7f28630a88f3db23aef891e4c0382f65cba
Pedro <pedrohenriquesb02@gmail.com>
2025-04-16 10:36:20 -0300

    Desenvolvendo os botões da classe MainScreen e testando o menu na classe UIMain

--- src/br/com/dio/ui/custom/screen/MainScreen.java
+++ src/br/com/dio/ui/custom/screen/MainScreen.java
@@ -1,6 +1,8 @@
 package br.com.dio.ui.custom.screen;
 
 import br.com.dio.service.BoardService;
+import br.com.dio.ui.custom.button.CheckGameStatusButton;
+import br.com.dio.ui.custom.button.FinishGameButton;
 import br.com.dio.ui.custom.button.ResetButton;
 import br.com.dio.ui.custom.frame.MainFrame;
 import br.com.dio.ui.custom.panel.MainPanel;
@@ -16,8 +18,9 @@ public class MainScreen {
     private final static Dimension dimension = new Dimension(600, 600);
     private final BoardService boardService;
 
+    private JButton checkGameStatusButton;
+    private JButton resetButton;
     private JButton finishGameButton;
-    private JButton checkGameButton;
 
     public MainScreen(final Map<String, String> gameConfig) {
         this.boardService = new BoardService(gameConfig);
@@ -36,16 +39,37 @@ public class MainScreen {
     }
 
     private void addFinishGameButton(final JPanel mainPanel) {
+        finishGameButton = new FinishGameButton(e -> {
+           if (boardService.gameIsFinished()) {
+               JOptionPane.showMessageDialog(null, "Parabéns, você concluiu o jogo");
+               resetButton.setEnabled(false);
+               checkGameStatusButton.setEnabled(false);
+               finishGameButton.setEnabled(false);
+           } else {
+               JOptionPane.showMessageDialog(null, "Seu jogo tem alguma inconsistência, ajuste e tente novamente");
+           }
+        });
         mainPanel.add(finishGameButton);
     }
 
     private void addCheckGameStatusButton(final JPanel mainPanel) {
-        mainPanel.add(checkGameButton);
+        checkGameStatusButton = new CheckGameStatusButton(e -> {
+            var hasErros = boardService.hasErrors();
+            var gameStatus = boardService.getStatus();
+            var message = switch (gameStatus) {
+                case NON_STARTED -> "O jogo não foi iniciado";
+                case INCOMPLETE -> "O jogo está incompleto";
+                case COMPLETE -> "O jogo está completo";
+            };
+            message += hasErros ? " e contém erros": " não contém erros";
+            JOptionPane.showMessageDialog(null, message);
+        });
+        mainPanel.add(MainScreen.this.checkGameStatusButton);
 
     }
 
     private void addResetButton(final JPanel mainPanel) {
-        JButton resetButton = new ResetButton(e -> {
+        resetButton = new ResetButton(e -> {
             var dialogResult = JOptionPane.showConfirmDialog(
                     null,
                     "Deseja realmente reiniciar o jogo?",
